c1e66f73de13933f7b420235de6f7514d5d124a7
Samir Nassar <samir@samirnassar.com>
2018-02-28 22:52:50 +0100

    bad project structure

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from app import app
+from flask import Flask
+
+app = Flask(__name__)
+
+from .core import app_setup
+
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=80)
