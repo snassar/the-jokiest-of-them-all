110b3ff8c903c52b2c573a33a689bfb2fc2f62d3
snassar <s.nassar@syseleven.de>
2018-03-02 12:03:07 +0100

    y u no db

--- cnkj/app.py
+++ cnkj/app.py
@@ -1,62 +1,36 @@
 from flask import Flask, jsonify, url_for, abort, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, Text
+from sqlalchemy import Column, Integer, Text, Table, create_engine, MetaData
 from datetime import datetime
 import os
 
 app = Flask(__name__)
 
-db = SQLAlchemy(app)
+engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+metadata = MetaData(bind=engine)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Joke(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    joke = db.Column(db.String(256), index=True, unique=True)
-
-    def __repr__(self):
-        return '<Joke {}>'.format(self.joke)
-
-class Tags(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tags = db.Column(db.String(128))
-    joke_id = db.Column(db.Integer, db.ForeignKey('joke.id'))
-
-    def __repr__(self):
-        return '<Tags {}>'.format(self.tags)
-
-class Rating(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    rating = db.Column(db.Integer)
-    joke_id = db.Column(db.Integer, db.ForeignKey('joke.id'))
-
-    def __repr__(self):
-        return '<Rating {}>'.format(self.rating)
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+jokes = Table('jokes', metadata, autoload=True)
+con = engine.connect()
+con.execute(jokes.insert(), id='1', joke='Chuck Norris has two speeds: Walk and Kill.')
 
 @app.route('/')
 def list_jokes():
 
 #    cursor.execute("show tables;")
-#    data = cursor.fetchall()
-#
-#    res = jsonify(data)
-#    return res
+    data = Jser.query.all()
+    res = jsonify(data)
+    return res
 
-    return '''
-<html>
-    <head>
-        <title>CNKJ</title>
-    </head>
-    <body>
-        <h1>CNKJ</h1>
-    </body>
-</html>
-'''
+#    return '''
+#<html>
+#    <head>
+#        <title>CNKJ</title>
+#    </head>
+#    <body>
+#        <h1>CNKJ</h1>
+#    </body>
+#</html>
+#'''
 
 @app.route('/<joke_id>')
 def show_joke(joke_id):
@@ -129,6 +103,10 @@ def random_joke():
     </body>
 </html>
 '''
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 if __name__ == '__main__':
     app.run("0.0.0.0", 5000)
